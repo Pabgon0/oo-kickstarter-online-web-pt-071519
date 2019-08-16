d9df01c3d786441a93bc1321c5b7bee9b2b7bb0e
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-16 00:50:24 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,9 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(project_name)
-    @backed_projects << project_name
-    Project.backers << self.name
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self.name
   end
   
   
