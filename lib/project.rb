e822d8c021271131d64d8d076868ed35f7dd4fa5
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-16 00:40:50 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
 attr_reader :title, :backers
 
-def initialize(title)
-  @title = title
-  @backers = []
-end
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
 
 
 
